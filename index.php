96574e6aab770c72c9dc8fb2425940fef3758d05
Ajit Kumar <ajitkumar22@gmail.com>
2025-06-20 13:00:38 +0000

    nav menu added

--- index.php
+++ index.php
@@ -86,6 +86,9 @@ if ($data = $messageform->get_data()) {
 $output = $PAGE->get_renderer('local_greetings');
 echo $output->header();
 
+// Adding simple navmenu acting as "tertiary navigation".Add commentMore actions
+echo $output->render_from_template('local_greetings/navmenu', []);
+
 if (isloggedin()) {
     echo local_greetings_get_greeting($USER);
 } else {
